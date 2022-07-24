ba4a0745d13e162c47a5f8cd78111928319a3549
sahil kulkarni <sahilkatwork@gmail.com>
2022-07-24 23:47:58 +0530

    exceptions

---
+++ hackerrank/easy_19-exceptions.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+attempts = int(input())
+for _ in range(attempts):
+    numerator, demoniator = (i for i in input().split(' '))    
+    try:        
+        _ = int(numerator)/int(demoniator)
+    except ZeroDivisionError as e:
+        print("Error Code:",e)
+    except ValueError as e:
+        print("Error Code:",e)
+    
